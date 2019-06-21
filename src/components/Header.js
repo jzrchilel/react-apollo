3f7b3eb7f21f0a36fea848a7c665dbeef89efb27
Jezer Chilel <jezer.chilel@gmail.com>
2019-06-20 23:03:06 -0500

    add Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
+import { AUTH_TOKEN } from '../constants';
 
-function Header() {
+function Header({ history }) {
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
   return(
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
@@ -10,10 +13,31 @@ function Header() {
         <Link to="/" className="ml1 no-underline black">
           new
         </Link>
-        <div className="ml1">|</div>
-        <Link to="/create" className="ml1 no-underline black">
-          submit
-        </Link>
+        {authToken && (
+          <div className="flex">
+            <div className="ml1">|</div>
+            <Link to="/create" className="ml1 no-underline black">
+              submit
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN)
+              history.push('/')
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link to="/login" className="ml1 no-underline black">
+            login
+          </Link>
+        )}
       </div>
     </div>
   )
